c5dfcc08b0b48ed7b237d12b4408824cd0e76299
Sanaz <szeraati69@gmail.com>
2024-02-28 22:44:54 +0330

    add todo and show alert

---
+++ js/app.js
@@ -0,0 +1,53 @@
+const taskInput = document.getElementById("task-input");
+const dateInput = document.getElementById("date-input");
+const addButton = document.getElementById("add-button");
+const alertMessage = document.getElementById("alert-message");
+
+// const todos = [];
+const todos = JSON.parse(localStorage.getItem("todos")) || [];
+console.log(todos);
+
+const generatedId = () => {
+  return Math.round(
+    Math.random() * Math.random() * Math.pow(10, 15)
+  ).toString();
+};
+
+const showAlert = (message, type) => {
+  alertMessage.innerHTML = "";
+  const alert = document.createElement("p");
+  (alert.innerText = message), alert.classList.add("alert");
+  alert.classList.add(`alert-${type}`);
+  alertMessage.append(alert);
+
+  setTimeout(() => {
+    alert.style.display = "none";
+  }, 2000);
+};
+
+const saveToLocalStorage = () => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const addHandler = () => {
+  const task = taskInput.value;
+  const date = dateInput.value;
+  const todo = {
+    id: generatedId(),
+    task,
+    date,
+    completed: false,
+  };
+  if (task) {
+    todos.push(todo);
+    saveToLocalStorage();
+    taskInput.value = "";
+    dateInput.value = "";
+    console.log(todos);
+    showAlert("Todo added successfully", "success");
+  } else {
+    showAlert("please enter a todo", "error");
+  }
+};
+
+addButton.addEventListener("click", addHandler);
